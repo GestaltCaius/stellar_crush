d84e4507b19be52d36cc69079c205a90da2dfc25
Guillaume <21450587@sun.ac.za>
2017-04-26 14:20:32 +0200

    Body objets done

--- GameObject.java
+++ GameObject.java
@@ -3,10 +3,27 @@ public class GameObject {
     private Vector r;
     private Vector v;
     private final double mass;
+    private final Color color;
+    private finale double radius;
 
-    public GameObject(Vector r, Vector v, double mass) {
+
+    public GameObject(Vector r, Vector v, double mass, Color color, double radius) {
     	this.r = r;
     	this.v = v;
     	this.mass = mass;
+    	this.color = color;
+    	this.radius = radius;
+    }
+
+    // calcultesForces from GameState has updated f, move then updates v and moves the GameObject
+    public move(Vector f, double dt) {
+    	Vector a = f.times(1/mass); // a = F/m
+    	v = v.plus(a.times(dt)); // updates velocity
+    	r = r.plus(v.times(dt)); // moves the GO
+    }
+
+    public draw() {
+    	StdDraw.setPenColor(color);
+    	StdDraw.filledCircle(r.data[0], r.data[1], radius);
     }
 }
